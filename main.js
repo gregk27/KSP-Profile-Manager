5618f3b411a069da19ee78a55fb9bbfe5d3150c7
Aree-Vanier <gregory@ajayinkingston.com>
2018-05-30 20:18:38 -0400

    added stylesheet, basic window functions work

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, ipcMain} = require("electron");
+
+var window;
 
 app.on('ready', function(){
-	var window = new BrowserWindow({width:640, height:480})
+	window = new BrowserWindow({width:640, height:480, frame: false})
 	window.loadFile("index.html")
 })
 app.on("browser-window-created",function(e,window) {
 	window.setMenu(null);
+	window.toggleDevTools();
 });
 
+
+
+
+ipcMain.on('window-min', function (){
+   window.minimize(); 
+});
+
+
+ipcMain.on('window-max', function (){
+   if (!window.isMaximized()) {
+	   window.maximize();          
+   } else {
+	   window.unmaximize();
+   }
+});
+
+ipcMain.on('window-close', function(){
+   console.log("close");
+   window.close();
+});
