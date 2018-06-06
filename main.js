2e03daa709a3482b07fb8fae9a093be9d5592a1c
Aree-Vanier <gregory@ajayinkingston.com>
2018-06-06 18:26:01 -0400

    Added profile name to ui

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ var profile = {
 
 //Load profile
 fs.readFile('profile.txt', 'utf-8' ,function(err, buf) {
-  profile=(JSON.parse(buf.toString()));
-  console.log(profile)
+	profile=(JSON.parse(buf.toString()));
+	console.log(profile)
 });
-	
-	
-	
-	
+
+
+
+
 var path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Kerbal Space Program\\KSP_x64.exe";//"C:\\Windows\\System32\\calc.exe";
 
 var window;
@@ -33,36 +33,36 @@ app.on("browser-window-created",function(e,window) {
 
 /*IPC window functions*/{
 ipcMain.on('window-min', function (){
-   window.minimize(); 
+	window.minimize();
 });
 
 ipcMain.on('window-max', function (){
-   if (!window.isMaximized()) {
-	   window.maximize();          
-   } else {
-	   window.unmaximize();
-   }
+	if (!window.isMaximized()) {
+		window.maximize();
+	} else {
+		window.unmaximize();
+	}
 });
 
 ipcMain.on('window-close', function(){
-   console.log("close");
-   window.close();
+	console.log("close");
+	window.close();
 });
 }
 
 ipcMain.on("window-launch", function(){
 	console.log("launch");
 	var execFile = require('child_process').execFile, child;
-         child = execFile(profile["path"], function(error,stdout,stderr) { 
-            if (error) {
-              //console.log(error.stack); 
-              //console.log('Error code: '+ error.code); 
-              //console.log('Signal received: '+ 
-              //       error.signal);
-              }
-              //console.log('Child Process stdout: '+ stdout);
-              //console.log('Child Process stderr: '+ stderr);
-          }); 
+	child = execFile(profile["path"], function(error,stdout,stderr) {
+		if (error) {
+			//console.log(error.stack);
+			//console.log('Error code: '+ error.code);
+			//console.log('Signal received: '+
+			//       error.signal);
+		}
+		//console.log('Child Process stdout: '+ stdout);
+		//console.log('Child Process stderr: '+ stderr);
+	});
 });
 
 ipcMain.on("window-profiles", function(){
@@ -75,7 +75,7 @@ ipcMain.on("set-profile", function(event, arg){
 	console.log(profile);
 	fs.writeFile('profile.txt', JSON.stringify(profile), function(err, data){
 		if (err) console.log(err);
-			console.log("Successfully Written to File.");
+		console.log("Successfully Written to File.");
 	});
 });
 
