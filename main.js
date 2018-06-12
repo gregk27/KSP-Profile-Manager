e608524fa10b0800d38f8e85305f52ca7b972fcc
Aree-Vanier <gregory@ajayinkingston.com>
2018-06-11 21:51:00 -0400

    relabled profile to config

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const {app, BrowserWindow, ipcMain} = require("electron");
 const fs = require("fs");
 
-var profile = {
-	"name":"Profile 1",
+var config = {
+	"name":"config 1",
 	"mode":"steam",
 	"version":"64",
 	"path":"C:\\Program Files\\KSP_x64"
 };
 
-//Load profile
-fs.readFile('profile.txt', 'utf-8' ,function(err, buf) {
-	profile=(JSON.parse(buf.toString()));
-	console.log(profile)
+//Load config
+fs.readFile('config.txt', 'utf-8' ,function(err, buf) {
+	config=(JSON.parse(buf.toString()));
+	console.log(config)
 });
 
 
@@ -21,7 +21,7 @@ var path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Kerbal Space Prog
 
 var window;
 
-var profileWindow;
+var configWindow;
 
 app.on('ready', function(){
 	window = new BrowserWindow({width:640, height:480, frame: false})
@@ -54,7 +54,7 @@ ipcMain.on('window-close', function(){
 ipcMain.on("window-launch", function(){
 	console.log("launch");
 	var execFile = require('child_process').execFile, child;
-	child = execFile(profile["path"], function(error,stdout,stderr) {
+	child = execFile(config["path"], function(error,stdout,stderr) {
 		if (error) {
 			//console.log(error.stack);
 			//console.log('Error code: '+ error.code);
@@ -66,20 +66,20 @@ ipcMain.on("window-launch", function(){
 	});
 });
 
-ipcMain.on("window-profiles", function(){
-	profileWindow = new BrowserWindow({width:320, height:240, frame: false});
-	window.loadFile('profiles.html');
+ipcMain.on("window-config", function(){
+	configWindow = new BrowserWindow({width:320, height:240, frame: false});
+	window.loadFile('config.html');
 });
 
-ipcMain.on("set-profile", function(event, arg){
-	profile = arg;
-	console.log(profile);
-	fs.writeFile('profile.txt', JSON.stringify(profile), function(err, data){
+ipcMain.on("set-config", function(event, arg){
+	config = arg;
+	console.log(config);
+	fs.writeFile('config.txt', JSON.stringify(config), function(err, data){
 		if (err) console.log(err);
 		console.log("Successfully Written to File.");
 	});
 });
 
-ipcMain.on("request-profile", function(event){
-	event.sender.send("get-profile", profile);
+ipcMain.on("request-config", function(event){
+	event.sender.send("get-config", config);
 })
